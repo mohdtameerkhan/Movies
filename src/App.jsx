57c12db96c63172ab8e11e770b3ad1f163f4e00b
mohdtameerkhan <khantameer087@gmail.com>
2026-05-16 20:21:05 +0300

    third

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@
 import {RouterProvider} from 'react-router-dom';
 import { Routes } from '/src/Routes/Routes.jsx';
 import './App.css'
-import { Suspense } from 'react';
-
-
 
 
 export function App() {
@@ -13,26 +10,7 @@ export function App() {
 
   return (
     <>
-
-    <Suspense fallback={
-        <div style={{
-          position: 'fixed',
-          top: 0, left: 0,
-          width: '100vw',
-          height: '100vh',
-          background: 'red',        // ← hard to miss
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: '50px',
-          color: 'white',
-          zIndex: 9999
-        }}>
-          LOADING
-        </div>
-      }>
         <RouterProvider router={Routes}  />
-      </Suspense>
      
     </>
     
--- src/Layout/HomeLayout.jsx
+++ src/Layout/HomeLayout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router"
 import { Navbar } from "../Navbar/Navbar"
 import {Footer} from '../component/Footer'
-import { Suspense } from "react"
+
 //import { LoaderSkeleton } from "../component/LoaderSkeleton"
 
 
